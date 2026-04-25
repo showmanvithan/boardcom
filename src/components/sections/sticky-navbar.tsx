3159e4d33f65fa7530dfcec5d1a3edbe05a61214
Vignesh T D <vigneshtd@outlook.com>
2026-04-25 11:39:39 +0530

    up

--- src/components/sections/sticky-navbar.tsx
+++ src/components/sections/sticky-navbar.tsx
@@ -6,14 +6,13 @@ import SwitchMode from "@/components/theme-toggle/switch-mode";
 type NavBarProfileNameProps = {
     name: string;
 }
-export default function StickyNavbar() {
-    const navbarprofileName: string = "vgnsh";
+export default function StickyNavbar({name} : NavBarProfileNameProps) {
     return (
                 <div className="sticky top-0 left-0 w-full  border border-dashed bg-zinc-200 dark:bg-black/80 flex pl-5 border-zinc-800 dark:border-zinc-500 backdrop-blur z-100">
                     <div className="flex items-center flex-1 justify-start">
                         <div className="flex items-center justify-center gap-1">
                             <Image src="/bug-game.png" alt="vignesh nav bar icon" width={25} height={25} className="dark:invert" />
-                            <TypographyH2>{navbarprofileName}</TypographyH2>
+                            <TypographyH2>{name}</TypographyH2>
                         </div>
                     </div>
                     <div className="flex-1 flex justify-end items-center -mb-1.5">
